df8c83010b5610972b25b83bc1e56a800a700bc4
beyondJohn <jpjpiesco@gmail.com>
2023-10-28 22:52:54 -0400

    initial commit

---
+++ BenchmarkDateConversion/DateParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BenchmarkDateConversion;
+
+public class DateParser
+{
+    public int GetYearFromDateTime(string dateTimeAsString)
+    {
+        var dateTime = DateTime.Parse(dateTimeAsString);
+        return dateTime.Year;
+    }
+
+}
---
+++ BenchmarkDateConversion/DateParserBenchmarks.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Attributes;
+namespace BenchmarkDateConversion;
+
+[MemoryDiagnoser]
+[Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class DateParserBenchmarks
+{
+    private const string DateTime = "2023-10-28T22:31:00Z";
+    private static readonly DateParser Parser = new DateParser();
+
+    [Benchmark]
+    public void GetYearFromDateTime(){
+        Parser.GetYearFromDateTime(DateTime);
+    }
+}
